b4129b6f1b5673affdbe06a4ed8c11329766d3a1
Agnieszka Sierbista <agnieszka.sierbista@gmail.com>
2023-02-24 16:04:00 +0100

    Query API endpoint for weather forecast using axios

--- src/epics.ts
+++ src/epics.ts
@@ -1,78 +1,37 @@
 import { combineEpics, Epic, ofType } from "redux-observable";
 import { EMPTY, of, switchMap } from "rxjs";
 import { SEARCH, searchSuccess } from "./actions";
+import axios from "axios";
 
 export const onInit: Epic = action$ => action$.pipe(
-	ofType("INIT"),
-	switchMap(() => {
-		console.log("INIT");
-		return EMPTY;
-	})
+  ofType("INIT"),
+  switchMap(() => {
+    console.log("INIT");
+    return EMPTY;
+  })
 );
 
 export const onSearch: Epic = action$ => action$.pipe(
-	ofType(SEARCH),
-	switchMap((action) => {
-
-		const data = action.payload === "London" ?
-			{
-				"coord": {
-					"lon": -0.1257,
-					"lat": 51.5085
-				},
-				"weather": [
-					{
-						"id": 804,
-						"main": "Clouds",
-						"description": "overcast clouds",
-						"icon": "04d"
-					}
-				],
-				"base": "stations",
-				"main": {
-					"temp": 3.16,
-					"feels_like": -0.47,
-					"temp_min": 1.49,
-					"temp_max": 4.34,
-					"pressure": 1012,
-					"humidity": 84
-				},
-				"visibility": 10000,
-				"wind": {
-					"speed": 4.12,
-					"deg": 230
-				},
-				"clouds": {
-					"all": 100
-				},
-				"dt": 1677228601,
-				"sys": {
-					"type": 2,
-					"id": 2075535,
-					"country": "GB",
-					"sunrise": 1677221845,
-					"sunset": 1677259830
-				},
-				"timezone": 0,
-				"id": 2643743,
-				"name": "London",
-				"cod": 200
-
-			}
-			:
-			{
-				"cod": "404",
-				"message": "city not found"
-			};
-
-
-
-		return of(searchSuccess(data));
-	})
+  ofType(SEARCH), switchMap((action) => {
+    return axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${action.payload}&appid=bad46dfee1ae1125ec4faf31e63449de&units=metric`)
+      .then(function(response) {
+        return response;
+      })
+      .catch(function(error) {
+        console.log(error);
+      })
+      .finally(function() {
+        // always executed
+      });
+  }),
+  switchMap((response) => {
+    const data = response?.data;
+    return of(searchSuccess(data));
+  })
 );
 
 export const rootEpic: Epic = combineEpics(
-	onInit,
-	onSearch
+  onInit,
+  onSearch
 );
 
