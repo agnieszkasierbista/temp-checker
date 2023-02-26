2d329e54ead11750b320aa4ef7b434a72874655c
Agnieszka Sierbista <agnieszka.sierbista@gmail.com>
2023-02-26 06:43:43 +0100

    Remove the delay before making the request

--- src/epics.ts
+++ src/epics.ts
@@ -1,12 +1,11 @@
 import { combineEpics, Epic, ofType } from "redux-observable";
-import { catchError, delay, from, map, of, switchMap } from "rxjs";
+import { catchError, from, map, of, switchMap } from "rxjs";
 import { SEARCH, searchFailed, searchSuccess } from "./actions";
 import axios, { AxiosResponse } from "axios";
 import { ResponseData } from "./types";
 
 export const onSearch: Epic = action$ => action$.pipe(
 	ofType(SEARCH),
-	delay(1000),
 	switchMap((action) => {
 		if (action.payload) {
 			const url = `https://api.openweathermap.org/data/2.5/weather?q=${action.payload}&appid=bad46dfee1ae1125ec4faf31e63449de&units=metric`;
